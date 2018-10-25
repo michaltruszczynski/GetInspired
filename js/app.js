d5aab267597ee02199c6a9ead848dc2d2bd51a75
michaltruszczynski <michal.truszczynski2@gmail.com>
2018-10-25 21:18:53 +0200

    drag drop

--- js/app.js
+++ js/app.js
@@ -1,7 +1,51 @@
 
 $(function() {
 
-	
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
 
+//fill Listeners
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+for (const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+  }
+
+//Drag functions
+
+function dragStart() {
+    console.log('start');
+    this.classList.add('hold');
+    setTimeout(()=> (this.className='invisible'),1);
+}
+
+function dragEnd() {
+    console.log('end');
+    this.className = "fill";
+}
+
+function dragOver (event) {
+    event.preventDefault();
+    
+}
+
+function dragEnter (event) {
+   event.preventDefault();
+   this.classList.add('hovered');
+}
+
+function dragLeave () {
+   this.classList.remove('hovered');
+}
+
+function dragDrop () {
+   this.className = 'empty';
+   this.append(fill)
+}
 
 })
